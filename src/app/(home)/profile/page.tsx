2831189294938888d77d4741b527c6ac1336d924
Heri Setyawan <herisetyawan233@gmail.com>
2024-05-02 15:50:46 +0700

    feat: add page card patient

--- src/app/(home)/profile/page.tsx
+++ src/app/(home)/profile/page.tsx
@@ -2,6 +2,9 @@ import Link from 'next/link'
 import { HiOutlineChevronRight } from "react-icons/hi2";
 import AppBar from '@/components/appbar/appbar'
 import Header from '@/components/sections/profile/header'
+import Image from 'next/image';
+import Right from  "@/assets/profile/right.png";
+import Left from  "@/assets/profile/left.png";
 
 function Profile() {
 
@@ -9,15 +12,19 @@ function Profile() {
     <main>
       <AppBar menu='Profile'/>
       <Header />
-      <div className='mx-5 px-4 py-2 bg-[#FAFAFA] mt-5 rounded-3xl grid grid-cols-2 divide-x'>
-        <div className="flex flex-col text-center">
-          <h3 className='text-[#001533] font-semibold text-base'>2</h3>
-          <p className='text-[10px] text-[#B7B7B7]'>Anggota Keluarga</p>
+      <div className="relative bg-[#FAFAFA] mx-5 mt-5 rounded-3xl overflow-hidden h-20">
+        <div className='absolute px-4 py-2 w-full mt-5 rounded-3xl overflow-hidden grid grid-cols-2 divide-x'>
+          <div className="flex flex-col text-center">
+            <h3 className='text-[#001533] font-semibold text-base'>2</h3>
+            <p className='text-[10px] text-[#B7B7B7]'>Anggota Keluarga</p>
+          </div>
+          <div className="flex flex-col text-center">
+            <h3 className='text-[#001533] font-semibold text-base'>6</h3>
+            <p className='text-[10px] text-[#B7B7B7]'>Kunjungan</p>
+          </div>
         </div>
-        <div className="flex flex-col text-center">
-          <h3 className='text-[#001533] font-semibold text-base'>6</h3>
-          <p className='text-[10px] text-[#B7B7B7]'>Kunjungan</p>
-        </div>
+        <Image src={Left} alt='' className='absolute bottom-0 right-0'/>
+        <Image src={Right} alt='' className='absolute top-0 left-0' />
       </div>
 
       <div className='flex flex-col gap-2 mt-5 mx-5'>
