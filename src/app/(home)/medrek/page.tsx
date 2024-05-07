3ac899461709baaf9bf825bbce4c004f8e8b578c
Heri Setyawan <herisetyawan233@gmail.com>
2024-05-07 16:15:08 +0700

    feat: dokter

--- src/app/(home)/medrek/page.tsx
+++ src/app/(home)/medrek/page.tsx
@@ -26,74 +26,74 @@ function Medrek() {
       </div>
 
       <div className="px-6 mt-3">
-      <div className="grid grid-cols-2 gap-2 mt-5">
-        <Link href={'/'} className="bg-white border px-2 py-2 rounded-md flex divide-x">
-          <div className="w-[40%] flex justify-center items-center">
-            <div className="bg-[#FFF1F1] w-10 h-10 rounded-full overflow-hidden flex justify-center items-center">
-              <Heart />
+        <div className="grid grid-cols-2 gap-2 mt-5">
+          <Link href={'/'} className="bg-white border px-2 py-2 rounded-md flex divide-x">
+            <div className="w-[40%] flex justify-center items-center">
+              <div className="bg-[#FFF1F1] w-10 h-10 rounded-full overflow-hidden flex justify-center items-center">
+                <Heart />
+              </div>
             </div>
-          </div>
-          <div className="px-2 w-[60%] flex justify-center items-center">
-            <p className="text-[10px] font-medium text-[#505050]">Poliklinik Jantung</p>
-          </div>
-        </Link>
-
-        <Link href={'/'} className="bg-white border px-2 py-2 rounded-md flex divide-x">
-          <div className="w-[40%] flex justify-center items-center">
-            <div className="bg-[#FFF1F1] w-10 h-10 rounded-full overflow-hidden flex justify-center items-center">
-              <Emphatize />
+            <div className="px-2 w-[60%] flex justify-center items-center">
+              <p className="text-[10px] font-medium text-[#505050]">Poliklinik Jantung</p>
             </div>
-          </div>
-          <div className="px-2 w-[60%] flex justify-center items-center">
-            <p className="text-[10px] font-medium text-[#505050]">Poliklinik Penyakit Dalam</p>
-          </div>
-        </Link>
+          </Link>
 
-        <Link href={'/'} className="bg-white border px-2 py-2 rounded-md flex divide-x">
-          <div className="w-[40%] flex justify-center items-center">
-            <div className="bg-[#FFF1F1] w-10 h-10 rounded-full overflow-hidden flex justify-center items-center">
-              <Lungs />
+          <Link href={'/'} className="bg-white border px-2 py-2 rounded-md flex divide-x">
+            <div className="w-[40%] flex justify-center items-center">
+              <div className="bg-[#FFF1F1] w-10 h-10 rounded-full overflow-hidden flex justify-center items-center">
+                <Emphatize />
+              </div>
             </div>
-          </div>
-          <div className="px-2 w-[60%] flex justify-center items-center">
-            <p className="text-[10px] font-medium text-[#505050]">Poliklinik Paru</p>
-          </div>
-        </Link>
-
-        <Link href={'/'} className="bg-white border px-2 py-2 rounded-md flex divide-x">
-          <div className="w-[40%] flex justify-center items-center">
-            <div className="bg-[#FFF1F1] w-10 h-10 rounded-full overflow-hidden flex justify-center items-center">
-              <Tooth />
+            <div className="px-2 w-[60%] flex justify-center items-center">
+              <p className="text-[10px] font-medium text-[#505050]">Poliklinik Penyakit Dalam</p>
             </div>
-          </div>
-          <div className="px-2 w-[60%] flex justify-center items-center">
-            <p className="text-[10px] font-medium text-[#505050]">Poliklinik Gigi & Mulut</p>
-          </div>
-        </Link>
+          </Link>
 
-        <Link href={'/'} className="bg-white border px-2 py-2 rounded-md flex divide-x">
-          <div className="w-[40%] flex justify-center items-center">
-            <div className="bg-[#FFF1F1] w-10 h-10 rounded-full overflow-hidden flex justify-center items-center">
-              <Stethoscope />
+          <Link href={'/'} className="bg-white border px-2 py-2 rounded-md flex divide-x">
+            <div className="w-[40%] flex justify-center items-center">
+              <div className="bg-[#FFF1F1] w-10 h-10 rounded-full overflow-hidden flex justify-center items-center">
+                <Lungs />
+              </div>
             </div>
-          </div>
-          <div className="px-2 w-[60%] flex justify-center items-center">
-            <p className="text-[10px] font-medium text-[#505050]">Poliklinik Umum</p>
-          </div>
-        </Link>
-
-        <Link href={'/'} className="bg-white border px-2 py-2 rounded-md flex divide-x">
-          <div className="w-[40%] flex justify-center items-center">
-            <div className="bg-[#FFF1F1] w-10 h-10 rounded-full overflow-hidden flex justify-center items-center">
-              <Hospital />
+            <div className="px-2 w-[60%] flex justify-center items-center">
+              <p className="text-[10px] font-medium text-[#505050]">Poliklinik Paru</p>
             </div>
-          </div>
-          <div className="px-2 w-[60%] flex justify-center items-center">
-            <p className="text-[10px] font-medium text-[#505050]">Instalasi Gawat Darurat</p>
-          </div>
-        </Link>
+          </Link>
 
-      </div>
+          <Link href={'/'} className="bg-white border px-2 py-2 rounded-md flex divide-x">
+            <div className="w-[40%] flex justify-center items-center">
+              <div className="bg-[#FFF1F1] w-10 h-10 rounded-full overflow-hidden flex justify-center items-center">
+                <Tooth />
+              </div>
+            </div>
+            <div className="px-2 w-[60%] flex justify-center items-center">
+              <p className="text-[10px] font-medium text-[#505050]">Poliklinik Gigi & Mulut</p>
+            </div>
+          </Link>
+
+          <Link href={'/'} className="bg-white border px-2 py-2 rounded-md flex divide-x">
+            <div className="w-[40%] flex justify-center items-center">
+              <div className="bg-[#FFF1F1] w-10 h-10 rounded-full overflow-hidden flex justify-center items-center">
+                <Stethoscope />
+              </div>
+            </div>
+            <div className="px-2 w-[60%] flex justify-center items-center">
+              <p className="text-[10px] font-medium text-[#505050]">Poliklinik Umum</p>
+            </div>
+          </Link>
+
+          <Link href={'/'} className="bg-white border px-2 py-2 rounded-md flex divide-x">
+            <div className="w-[40%] flex justify-center items-center">
+              <div className="bg-[#FFF1F1] w-10 h-10 rounded-full overflow-hidden flex justify-center items-center">
+                <Hospital />
+              </div>
+            </div>
+            <div className="px-2 w-[60%] flex justify-center items-center">
+              <p className="text-[10px] font-medium text-[#505050]">Instalasi Gawat Darurat</p>
+            </div>
+          </Link>
+
+        </div>
       </div>
     </main>
   )
