ddb4a44574b6c6ec75aa3114f81aa69e8483f069
Heri Setyawan <herisetyawan233@gmail.com>
2024-06-02 18:58:01 +0700

    bug: home card docter

--- src/components/sections/home/dokter.tsx
+++ src/components/sections/home/dokter.tsx
@@ -5,30 +5,31 @@ import Bagus from "@/assets/dokter/bagus.png";
 import Riana from "@/assets/dokter/riana.png";
 import Mulfi from "@/assets/dokter/mulfi.png";
 import CardDokter from "@/components/card/dokter";
+import dokter from "@/constant/data/dokter";
 
 function Dokter() {
   return (
     <>
-        <div className="flex justify-between mt-5">
-            <h1 className="font-bold">Dokter Kami</h1>
-            <Link href={'/promise'} className="text-[#F7490D] font-medium text-sm">Lihat Semua</Link>
-        </div>
-        <div className="grid grid-cols-2 gap-5 mt-5">
-            <CardDokter image={Nindya}>
-                <p className="text-[10px] mt-3 text-center">drg. Nindya Puspita Ayu</p>
+      <div className="flex justify-between mt-5">
+        <h1 className="font-bold">Dokter Kami</h1>
+        <Link href={"/promise"} className="text-[#F7490D] font-medium text-sm">
+          Lihat Semua
+        </Link>
+      </div>
+      <div className="grid grid-cols-2 gap-5 mt-5">
+        {dokter
+          .filter((_, index) => index < 4)
+          .map((person, index) => (
+            <CardDokter image={person.photo} key={index} id={index + 1}>
+              <p className="text-[10px] mt-3 text-center">{person.name}</p>
+              <p className="text-[10px] text-center text-red-600">
+                {person.specialist}
+              </p>
             </CardDokter>
-            <CardDokter image={Bagus}>
-                <p className="text-[10px] mt-3 text-center">drg. Nindya Puspita Ayu</p>
-            </CardDokter>
-            <CardDokter image={Riana}>
-                <p className="text-[10px] mt-3 text-center">drg. Nindya Puspita Ayu</p>
-            </CardDokter>
-            <CardDokter image={Mulfi}>
-                <p className="text-[10px] mt-3 text-center">drg. Nindya Puspita Ayu</p>
-            </CardDokter>
-        </div>
+          ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default Dokter
+export default Dokter;
