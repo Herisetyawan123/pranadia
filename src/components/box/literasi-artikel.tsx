18fa7e357e44ec555a9801b255cf962a5449e2c5
Heri Setyawan <herisetyawan233@gmail.com>
2024-06-02 20:01:00 +0700

    feat: add articel and page all articel

--- src/components/box/literasi-artikel.tsx
+++ src/components/box/literasi-artikel.tsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
-function BoxLiterasiArtikel({ src }: { src: string }) {
+function BoxLiterasiArtikel({
+  src,
+  title,
+  category,
+}: {
+  src: string;
+  title: string;
+  category: string;
+}) {
   return (
-    <div className="bg-blue-500 rounded-lg h-52" style={{
-        'background': `url(${src})`,
-        'backgroundRepeat': 'no-repeat',
-        'backgroundSize': 'cover'
-    }}>
-
-    </div>
-  )
+    <Link href={"/"}>
+      <div
+        className="bg-blue-500 rounded-lg h-52 flex relative overflow-hidden"
+        style={{
+          background: `url(${src})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <div
+          className="w-full h-1/2 bg-white absolute bottom-0 px-2 flex flex-col justify-end py-2 gap-2"
+          style={{
+            background:
+              "linear-gradient(181.46deg, rgba(0, 0, 0, 0) 1.24%, #D41F13 98.75%)",
+          }}
+        >
+          <span className="bg-red-50 text-red-500 w-fit px-3 rounded-full text-xs">
+            {category}
+          </span>
+          <p className="text-xs text-white font-medium line-camp-2">{title}</p>
+        </div>
+      </div>
+    </Link>
+  );
 }
 
-export default BoxLiterasiArtikel
+export default BoxLiterasiArtikel;
--- src/components/sections/home/literasi-artikel.tsx
+++ src/components/sections/home/literasi-artikel.tsx
@@ -1,22 +1,34 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 // asset
 import lifestyle from "@/assets/literasi/life-style.png";
-import BoxLiterasiArtikel from '@/components/box/literasi-artikel';
+import BoxLiterasiArtikel from "@/components/box/literasi-artikel";
+
+import articels from "@/constant/data/artikel";
 
 function LiterasiArtikel() {
   return (
     <>
-        <div className="flex justify-between mt-5">
-            <h1 className="font-bold">Literasi & Artikel</h1>
-            <Link href={'/'} className="text-[#F7490D] font-medium text-sm">Lihat Semua</Link>
-        </div>
-        <div className="grid grid-cols-2 gap-5 mt-5">
-            <BoxLiterasiArtikel src={lifestyle.src} />
-            <BoxLiterasiArtikel src={lifestyle.src} />
-        </div>
+      <div className="flex justify-between mt-5">
+        <h1 className="font-bold">Literasi & Artikel</h1>
+        <Link href={"/articels"} className="text-[#F7490D] font-medium text-sm">
+          Lihat Semua
+        </Link>
+      </div>
+      <div className="grid grid-cols-2 gap-5 mt-5">
+        {articels
+          .filter((_, index) => index < 2)
+          .map((articel, index) => (
+            <BoxLiterasiArtikel
+              key={index}
+              src={articel.thumbnail}
+              title={articel.title}
+              category={articel.category}
+            />
+          ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default LiterasiArtikel
+export default LiterasiArtikel;
