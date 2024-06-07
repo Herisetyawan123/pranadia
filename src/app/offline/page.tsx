017ea2510d6275881ed99dc08f6bba03928f4fa5
Heri Setyawan <herisetyawan233@gmail.com>
2024-06-08 04:45:01 +0700

    check offline again (installSewist again) custom page

--- src/app/offline/page.tsx
+++ src/app/offline/page.tsx
@@ -2,9 +2,11 @@ import AppBarBack from "@/components/appbar/appbar";
 
 export default function Page() {
   return (
-    <main className="w-full h-full flex justify-center items-center bg-white text-black">
+    <main className="">
       <AppBarBack menu="Offline" />
-      <h1 className="font-semibold text-lg">No Internet!</h1>
+      <div className="w-full h-full flex justify-center items-center bg-white text-black">
+        <h1 className="font-semibold text-lg">No Internet!</h1>
+      </div>
     </main>
   );
 }
